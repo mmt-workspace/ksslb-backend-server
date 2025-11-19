c2503161207ce38d1fb81f8219a42a8e2bdd64b8
skygifted <abdulazeezibrahim202@gmail.com>
2025-11-19 11:37:25 +0100

    #

--- app/applicants/SignUp/UploadDoc/UploadDocument.js
+++ app/applicants/SignUp/UploadDoc/UploadDocument.js
@@ -109,9 +109,9 @@ const GetUploadDocumentByUserToken = (req, res) => {
 
     const { user_token,qualification_type,ref_doc_token } = req.params;
 
-    const sql = `SELECT * FROM applicant_doc WHERE user_token = ? AND qualification_type = ? AND ref_doc_token = ?;`;
+    const sql = `SELECT * FROM applicant_doc WHERE user_token = ? AND qualification_type = ? OR qualification_type = ? AND ref_doc_token = ?;`;
 
-    db.query(sql, [user_token,qualification_type,ref_doc_token], (err, result) => {
+    db.query(sql, [user_token,qualification_type,"both",ref_doc_token], (err, result) => {
 
         if (err) {
             console.log(err);
